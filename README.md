106e75f185216c4cbe7698d7f5273743d33ac775
per <per@alipsa.se>
2022-05-04 21:41:44 +0200

    WIP implement jwt test

--- README.md
+++ README.md
@@ -28,13 +28,14 @@ Add the following dependency to your maven pom.xml (or equivalent for your build
  * @throws com.fasterxml.jackson.core.JsonProcessingException if deserialization from String to a SearchResult failed
  */
   private SearchResult search(@NotBlank String question) throws RestException, JsonProcessingException {
-    String query = URLEncoder.encode(question, StandardCharsets.UTF_8);
+    String query = java.net.URLEncoder.encode(question, StandardCharsets.UTF_8);
     String url = "https://www.googleapis.com/customsearch/v1" +
         "?key=theLongAPIkey" +
         "&cx=theSearchEngineId" +
         "&searchType=image" +
         "&q=" + query +
         "&num=" + 9;
+    var restClient = new se.alipsa.simplerest.RestClient();  
     return restClient.get(url).getObject(SearchResult.class);
   }
 ```
@@ -42,6 +43,9 @@ Add the following dependency to your maven pom.xml (or equivalent for your build
 ## JSON web token example
 
 ```groovy
+import static se.alipsa.simplerest.CommonHeaders.*
+import se.alipsa.simplerest.*;
+import java.util.*;
 // 1. First log in to the server to get a JWT token
 
 /**
@@ -49,19 +53,20 @@ Add the following dependency to your maven pom.xml (or equivalent for your build
  * using com.auth0:java-jwt to create and authenticate subsequent requests according to the JWT standard
  * @param userName
  * @param password
- * @return a JWT token if sucessful, otherwise null
+ * @return a JWT token if successful, otherwise null
  */
 String login(String userName, String password) throws RestException {
-    java.util.Map<String, String> loginInfo = new java.util.HashMap<>();
-    loginInfo.put("username", userName);
-    loginInfo.put("password", password);
-    se.alipsa.simplerest.Response response = restClient.post("https://localhost:8080/api/login", loginInfo);
-    System.out.println("Login: response code: " + response.getResponseCode());
-    System.out.println("Got headers: " + response.getHeaders());
-    if (response.getResponseCode() == 200) {
-        return response.getPayload().trim();
-    }
-    return null;
+  var loginInfo = new HashMap<String, String>();
+  loginInfo.put("username", userName);
+  loginInfo.put("password", password);
+  var restClient = new RestClient();
+  Response response = restClient.post("https://localhost:8080/api/login", loginInfo);
+  System.out.println("Login: response code: " + response.getResponseCode());
+  System.out.println("Got headers: " + response.getHeaders());
+  if (response.getResponseCode() == 200) {
+    return response.getPayload().trim();
+  }
+  return null;
 }
 
 // 2. Then you can use the JWT token in subsequent requests e.g:
@@ -72,21 +77,42 @@ String login(String userName, String password) throws RestException {
  * @return a List of Companies
  */
 List<Company> getCompanies(String jwtToken) {
-    String token = "Bearer " + jwtToken;
-    try {
-        // Add the token to the header of the GET request
-        Response response = restClient.get("https://localhost:8080/api/company", Map.of("Authorization", token));
-        // getObjectList return a list of the type passed in as a parameter when the payload is a JSON array or Objects
-        return response.getObjectList(Company.class);
-    } catch (RestException e) {
-        System.err.println("Failed to talk to server: " + e);
-    } catch (JsonProcessingException e) {
-        System.err.println("Failed to deserialize token: " + e);
-    }
-    return Collections.emptyList();
+  String token = "Bearer " + jwtToken;
+  var restClient = new RestClient();
+  try {
+    // Add the token to the header of the GET request
+    Response response = restClient.get(
+            "https://localhost:8080/api/company", 
+            Map.of("Authorization", token)
+    );
+    // getObjectList return a list of the type passed in as a parameter 
+    // when the payload is a JSON array or Objects
+    return response.getObjectList(Company.class);
+  } catch (RestException e) {
+    System.err.println("Failed to talk to server: " + e);
+  } catch (JsonProcessingException e) {
+    System.err.println("Failed to deserialize token: " + e);
+  }
+  return Collections.emptyList();
 }
 ```
 
 ## Basic authentication example
+
+Basic auth is simply just a matter of adding the Authorization header for each request, e.g:
+
 ```groovy
+import static se.alipsa.simplerest.CommonHeaders.*
+import se.alipsa.simplerest.*;
+
+// Company is an arbitrary data object
+var company = new Company();
+company.setName("Creative Design");
+var restClient = new RestClient();
+var response = restClient.post(
+    "http://localhost:8080/companies/company",
+    company,
+    Map.of(AUTHORIZATION, basicAuth("myUserName", "myPassword"))
+);
+Company c = response.getObject(Company.class);
 ```
--- src/test/java/test/alipsa/simplerest/JwtRestTest.java
+++ src/test/java/test/alipsa/simplerest/JwtRestTest.java
@@ -13,22 +13,27 @@ import se.alipsa.simplerest.RestException;
 import test.alipsa.simplerest.model.Company;
 import test.alipsa.simplerest.servlets.JwtServlet;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
-import static se.alipsa.simplerest.CommonHeaders.CONTENT_LENGTH;
-import static se.alipsa.simplerest.CommonHeaders.CONTENT_TYPE;
+import static se.alipsa.simplerest.CommonHeaders.*;
+import static se.alipsa.simplerest.CommonHeaders.basicAuth;
 
 public class JwtRestTest {
 
   private static Server server;
   private static String serverUrl;
   private static RestClient restClient;
+  private static String userName = "per";
+  private static String password = "secret";
 
-
+  private static String jwtToken = null;
 
   @BeforeAll
   public static void startJetty() throws Exception {
-    System.out.println("Starting jetty server");
+    System.out.println("Starting jwt jetty server");
     server = new Server();
     ServerConnector connector = new ServerConnector(server);
     connector.setPort(0); // auto-bind to available port
@@ -57,53 +62,96 @@ public class JwtRestTest {
       fail(e);
     }
   }
-  
+
+  String getToken() throws RestException {
+    if (jwtToken == null) {
+      jwtToken = login();
+    }
+    return jwtToken;
+  }
+
+  String login() throws RestException {
+    var loginInfo = new HashMap<String, String>();
+    loginInfo.put("username", userName);
+    loginInfo.put("password", password);
+    var restClient = new RestClient();
+    Response response = restClient.post(serverUrl + "jwt/login", loginInfo);
+    System.out.println("Login: response code: " + response.getResponseCode());
+    System.out.println("Got headers: " + response.getHeaders());
+    if (response.getResponseCode() == 200) {
+      return "Bearer " + response.getPayload().trim();
+    }
+    return null;
+  }
+
   @Test
-  public void simpleGetTest() throws RestException, JsonProcessingException {
-    var response = restClient.get(serverUrl + "simple");
-    assertEquals(200, response.getResponseCode(), "get /simple, response Code");
+  public void jwtGetTest() throws RestException, JsonProcessingException {
+    var response = restClient.get(serverUrl + "jwt",
+        Map.of(AUTHORIZATION, getToken())
+    );
+    assertEquals(200, response.getResponseCode(), "get /jwt, response Code");
     Company company = response.getObject(Company.class);
     assertEquals("ABC", company.getName(), "Company name");
     assertEquals(123, company.getNumber(), "company number");
   }
 
   @Test
-  public void simplePostTest() throws RestException, JsonProcessingException {
+  public void jwtPostTest() throws RestException, JsonProcessingException {
     Company company = new Company();
     company.setName("Creative Design");
-    var response = restClient.post(serverUrl + "simple", company);
+    var response = restClient.post(
+        serverUrl + "jwt",
+        company,
+        Map.of(AUTHORIZATION, getToken())
+    );
     Company c = response.getObject(Company.class);
     assertEquals("Creative Design", c.getName(), "Company name");
     assertEquals(191919, c.getNumber(), "company number");
   }
 
   @Test
-  public void simplePutTest() throws JsonProcessingException, RestException {
+  public void jwtPutTest() throws JsonProcessingException, RestException {
     Company company = new Company("Creative Design", 191919);
-    var response = restClient.put(serverUrl + "simple", company);
+    var response = restClient.put(
+        serverUrl + "jwt",
+        company,
+        Map.of(AUTHORIZATION, getToken())
+    );
     Company c = response.getObject(Company.class);
     assertEquals("Creative Design", c.getName(), "Company name");
     assertEquals(191919, c.getNumber(), "company number");
   }
 
   @Test
-  public void simpleDeleteTest() throws RestException {
-    var response = restClient.delete(serverUrl + "simple/company/123");
-    assertEquals(204, response.getResponseCode(), "delete /simple/company/123, response Code");
-    response = restClient.delete(serverUrl + "simple/company/345");
-    assertEquals(404, response.getResponseCode(), "delete /simple/company/345, response Code");
+  public void jwtDeleteTest() throws RestException {
+    var response = restClient.delete(
+        serverUrl + "jwt/company/123",
+        Map.of(AUTHORIZATION, getToken())
+    );
+    assertEquals(204, response.getResponseCode(), "delete /jwt/company/123, response Code");
+    response = restClient.delete(
+        serverUrl + "jwt/company/345",
+        Map.of(AUTHORIZATION, getToken())
+    );
+    assertEquals(404, response.getResponseCode(), "delete /jwt/company/345, response Code");
   }
 
   @Test
-  public void simpleHeadTest() throws RestException {
-    Response response = restClient.head(serverUrl + "simple");
+  public void jwtHeadTest() throws RestException {
+    Response response = restClient.head(
+        serverUrl + "jwt",
+        Map.of(AUTHORIZATION, getToken())
+    );
     assertEquals("27", response.getHeader(CONTENT_LENGTH), "Content-Length");
     assertEquals("application/json", response.getHeader(CONTENT_TYPE));
   }
 
   @Test
-  public void simpleOptionsTest() throws RestException {
-    Response response = restClient.options(serverUrl + "simple");
+  public void jwtOptionsTest() throws RestException {
+    Response response = restClient.options(
+        serverUrl + "jwt",
+        Map.of(AUTHORIZATION, getToken())
+    );
     System.out.println(response.getHeaders());
   }
 }
